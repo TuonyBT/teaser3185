1367f6b7a8896b323eff2aa09d476fb3c43b5cf9
tuonybt <tonybt.tbt@gmail.com>
2023-10-10 09:28:39 +0100

    Solution found but rotations allowed

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
 use itertools::Itertools;
-use std::{collections::HashSet, vec};
+use std::{collections::{HashSet, BTreeMap}, vec};
 
 const Y_PICK: [[usize; 3]; 3] = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
 
 fn main() {
-
-    let mut triplets = Vec::<HashSet<u8>>::new();
-
-    for a in 1u8..5 {
-        for b in (a + 1)..(16 - a) / 2 {
-            let triplet = HashSet::from([a, b, 15 - a - b]);
-            triplets.push(triplet);
-        }
-    }
-//    println!("triplets: {:?}", triplets);
-
-    let mut squares = Vec::<Vec<&HashSet<u8>>>::new();
-
-    for (first_row, second_row) in triplets.iter().cartesian_product(&triplets) {
-        let used = first_row.union(&second_row).map(|&z| z).collect::<HashSet<u8>>();
-        if used.len() < 6 {continue;}
-        let unused = triplets.iter().filter(|z| z.is_disjoint(&used)).collect::<Vec<&HashSet<u8>>>();
-        if unused.len() == 0 {continue;}
-
-        for third_row in unused.into_iter() {
-            squares.push(vec![first_row, second_row, third_row]);
-        }
-    }
-//    println!("Squares {:?}", squares);
 
 //  Define the square as a central cell x and 4 complementary pairs (y, z) to form the sequences that pass through x.
 //  y_i < z_i; y_i != y_j; z_i != z_j
@@ -46,14 +22,27 @@ fn main() {
 
 //  for each x, find triplets (y_i, z_j, z_k) of complementary pairs such that their sum is 15
 
+    let mut squares_dict = BTreeMap::<i32, Vec<Vec<Vec<u8>>>>::new();
+    let mut totals = Vec::<i32>::new();
+
     for (x, yz) in yz_vec.into_iter().enumerate() {
         for triplet in yz.iter().combinations(3) {
-            let top_rows = Y_PICK.iter().map(|idx| 
+            let top_row_pick = Y_PICK.iter().map(|idx| 
                                                     idx.iter().zip(triplet.to_owned()).map(|(&ix, &yz)| yz[ix])
                                                                 .collect::<Vec<u8>>())
                                             .filter(|t| t.iter().sum::<u8>() == 15)
                                             .collect::<Vec<Vec<u8>>>();
-            if top_rows.len() == 0 {continue;}
+            if top_row_pick.len() == 0 {continue;}
+
+            let top_rows = top_row_pick.iter().map(|sel| 
+                                            (&sel.clone().into_iter().sorted().permutations(3).collect_vec()[..3]).to_owned())
+                                            .flatten()
+                                            .collect::<Vec<Vec<u8>>>();
+
+//            let top_rows = top_row_pick.clone();
+
+
+
             let bot_rows = top_rows.iter().map(|candidate| 
                                                             candidate.iter().rev().map(|yz| 15_u8 - x as u8 - yz).collect::<Vec<u8>>())
                                                         .collect::<Vec<Vec<u8>>>();
@@ -62,7 +51,7 @@ fn main() {
                                                                                     .collect::<Vec<i8>>())
                                                         .collect::<Vec<Vec<i8>>>();
             let valid_mid_rows = mid_rows.iter().enumerate()
-                                                    .filter(|test| test.1.iter().all(|x| x >= &0))
+                                                    .filter(|test| test.1.iter().all(|x| x >= &0) && test.1.iter().sum::<i8>() == 15)
                                                     .collect::<Vec<(usize, &Vec<i8>)>>();
 
             if valid_mid_rows.len() == 0 {continue;}
@@ -74,19 +63,21 @@ fn main() {
 //            println!("Bot Rows {:?}", bot_rows);
 //            println!("Valid Mid Rows {:?}", valid_mid_rows);
 
+
             for (i, _v) in valid_mid_rows {
                 let square = vec![top_rows[i].to_owned(), mid_rows[i].iter().map(|&x| x as u8).collect::<Vec<u8>>(), bot_rows[i].to_owned()];
-                if square.iter().flatten().collect::<HashSet<&u8>>().len() < 9 {continue;}
-                println!("     Possible Magic Square {:?}", square);
-
-//  Need to swap y with one of the z's to get more squares that are not reflections or rotations
-// *********************************************************************************************
-
+                let members = square.iter().flatten().collect::<HashSet<&u8>>();
+                if members.len() < 9 {continue;}
+                let total = members.iter().map(|&&p| 2_i32.pow(p as u32)).sum::<i32>();
+                totals.push(total);
+                squares_dict.entry(total).or_insert(Vec::<Vec<Vec<u8>>>::new()).push(square);
             }
-            println!();
-
         }
-
     }
+    totals.sort();
 
+    println!("The totals in ascending order are:");
+    for (total, squares) in squares_dict {
+        println!("Total {} with squares {:?}", total, squares);
+    }
 }
